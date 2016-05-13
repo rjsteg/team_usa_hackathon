56cd3d49fca1b0929299b118dfa84396eb3c8e07
Brandon Thimmesch <brandonthimmesch@gmail.com>
2016-05-13 12:40:19 -0500

    Added files from SWO hackathon

---
+++ HelloHackathon/HelloHackathon/Controllers/PracticeManager/PracticeManagerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloHackathon.Controllers.PracticeManager
+{
+    public class PracticeManagerController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            using()
+
+            return View();
+        }
+    }
+}
